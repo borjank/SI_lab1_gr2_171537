be7a8c1ee642d07e469b02645f66476fa225319a
borjank <63057459+borjank@users.noreply.github.com>
2020-04-09 17:45:20 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,17 +1,65 @@
+import java.util.List;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 	List<Integer> grades;
-	//TODO constructor
+	public Student(String index, String firstName, String lastName, List<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
+	
+	public String getIndex() {
+		return index;
+	}
 
-	//TODO seters & getters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public List<Integer> getGrades() {
+		return grades;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGrades(List<Integer> grades) {
+		this.grades = grades;
+	}
 
 	public double getAverage() {
-		//TODO
+		double avg=0,c=0;
+		for(int grade : grades) {
+			avg+=grade;
+			c++;
+		}
+		return (avg/c);
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		int ects=0;
+		for(int grade: grades) {
+			if(grade>5) {
+				ects+=6;
+			}
+		}
+		return ects;
 	}
 }
